0736dc3195a1132498afa80be84b1133137edbc6
chloe <312864100@qq.com>
2022-02-26 15:01:20 -0500

    made changes after solution

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,11 +1,17 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = ({ videos, handleVideoListEntryTitleClick }) => (
+
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {videos.map(video =>
+      <VideoListEntry
+        key={video.etag}
+        video={video}
+        handleVideoListEntryTitleClick={handleVideoListEntryTitleClick}
+      />
+    )}
   </div>
+
 );
 
 // PropTypes tell other developers what `props` a component expects
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import { API_KEY, YOUTUBE_API_KEY } from '../config/config.js';
 
 $.ajaxPrefilter(function (settings, _, jqXHR) {
@@ -5,7 +7,16 @@ $.ajaxPrefilter(function (settings, _, jqXHR) {
 });
 
 var searchYouTube = (query, callback) => {
-  // TODO
+  $.get('https://app-hrsei-api.herokuapp.com/api/recastly/videos', {
+    youtube_api_key: YOUTUBE_API_KEY,
+    q: query
+  }).done((items) => {
+    if (callback) {
+      callback(items);
+    }
+  }).fail(({ responseJSON }) => {
+    responseJSON.error.errors.forEach((err) => console.error(err));
+  });
 };
 
 export default searchYouTube;
